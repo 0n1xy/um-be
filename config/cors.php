2cf681bef8931135e360cde9c887eeead872c533
0n1xy <167971036+0n1xy@users.noreply.github.com>
2025-03-13 20:25:26 +0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'], 
-    'allowed_origins' => ['http://localhost:5173', 'https://um-fe.vercel.app'], 
+    'allowed_origins' => ['*'], 
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'], 
     'exposed_headers' => ['Authorization', 'X-CSRF-TOKEN'],
