6dd289cb49d188c29356250af2c731f4d36f8042
0n1xy <167971036+0n1xy@users.noreply.github.com>
2025-03-13 19:39:40 +0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => [ 'api/*'], // Cho phép CORS cho tất cả các API
     'allowed_methods' => ['*'], // Cho phép tất cả phương thức (GET, POST, PUT, DELETE...)
-    'allowed_origins' => ['http://localhost:5173'], // Chỉ cho phép truy cập từ React frontend
+    'allowed_origins' => ['http://localhost:5173', 'https://um-fe.vercel.app/'], // Chỉ cho phép truy cập từ React frontend
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'], // Cho phép tất cả headers
     'exposed_headers' => [],
